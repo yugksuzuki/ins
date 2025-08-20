52d21da07291f4f04146e0290611eb709768aa82
yugksuzuki <g-suzuki@hotmail.com>
2025-08-20 16:28:43 -0300

    amem 12

---
+++ api/auth.js
@@ -0,0 +1,4 @@
+// api/auth.js
+// Encaminha /api/auth para o seu app Express
+const app = require('../app');
+module.exports = app; // NÃO dar app.listen aqui
---
+++ api/auth/callback.js
@@ -0,0 +1,4 @@
+// api/auth/callback.js
+// Encaminha /api/auth/callback para o seu app Express
+const app = require('../../app');
+module.exports = app; // idem acima
