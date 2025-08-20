1f5ab3f1f903d13ec6ed04d091275652a908d484
yugksuzuki <g-suzuki@hotmail.com>
2025-08-19 21:49:34 -0300

    e

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+// models/User.js
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
@@ -10,7 +11,7 @@ userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
-}, { timestamps: true });
+});
 
 userSchema.methods.comparePassword = function (candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
